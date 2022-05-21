7fbe9d56e6cebc44c4fa780a48002083e4d271f1
chandansah0 <chandanprasadsah9@gmail.com>
2022-05-21 07:44:43 +0545

    diff_type

---
+++ 3_Introduction/pass_by_reference.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void refer (int a)
+{
+    a=10;
+    cout<<"the value is:"<<a<<endl;
+}
+void normal(int b){
+    b=20;
+    cout<<"the value is:"<<b<<endl;
+}
+int main(){
+   int  a=15;
+   int b=25;
+    refer(a);
+    normal(b);
+    cout<<"the value inside main of a is:"<<a<<endl;
+    cout<<"the value inside main of b is:"<<b<<endl;
+}
+
