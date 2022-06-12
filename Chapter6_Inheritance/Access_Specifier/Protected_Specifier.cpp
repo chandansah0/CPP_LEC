0b7341d3d860989b27800df9abfbb4a153c5caf9
chandansah0 <chandanprasadsah9@gmail.com>
2022-06-12 22:14:26 +0545

    protected_specifier

--- Chapter6_Inheritance/Access_Specifier/Protected_Specifier.cpp
+++ Chapter6_Inheritance/Access_Specifier/Protected_Specifier.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
 using namespace std;
-//base class
-class parent
-{
-    //protected data members
-    protected:
+class Parent{
+
+public:
     int id_protected;
 };
-//sub class or derived from public base class
-class child: public parent
+class child : public Parent
 {
     public:
-    void setid(int id)
-    {
-        //child class is able to access the inherited
-        //protected data members of base class
-        id_protected = id ;
-
+    void setId(int id){
+    id_protected = id;
     }
-    void displayId()
-    {
-        cout<<"Id protected is :"<<id_protected<<endl;
 
-    }
+void dispalyId(){
+    cout<< "id_protected is: "<<id_protected<<endl;
+
+}
+
 };
-//mian function 
 int main(){
     child obj1;
-    //member function of the derived class can
-    //access the protected data members of the base class
     obj1.setId(81);
-    obj1.displayId();
+    obj1.dispalyId();
     return 0;
 }
 
