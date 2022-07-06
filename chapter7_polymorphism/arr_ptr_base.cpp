d129e86df3d8735713c8c9e1a53a33d43bb8c2df
chandansah0 <chandanprasadsah9@gmail.com>
2022-07-06 22:17:41 +0545

    using abstract and access them using base class pointer

---
+++ chapter7_polymorphism/arr_ptr_base.cpp
@@ -0,0 +1,58 @@
+//example of array of pointer to base class object
+
+#include <iostream>
+using namespace std;
+class point
+{
+    public:
+    virtual void draw()
+    {
+        cout<<"point"<<endl;
+    }
+};
+class Line:public point
+{
+    public:
+    void draw()
+    {
+        cout<<"Line"<<endl;
+    }
+};
+class Triangle:public point
+{
+    public:
+    void draw()
+    {
+        cout<<"Triangle"<<endl;
+    }
+};
+class Polygon: public point
+{
+    public:
+    void draw()
+    {
+        cout<<"Polygon"<<endl;
+
+    }
+};
+class Circle:public point 
+{
+    public:
+    void draw()
+    {
+        cout<<"Circle"<<endl;
+    }
+};
+int main()
+{
+    point pt;
+    Line ln;
+    Triangle tr;
+    Polygon py;
+    Circle cr;
+
+    point *baseptr[]={&pt,&ln,&tr,&py,&cr};
+    cout<<"Figure drawn by base pointer are:"<<endl;
+    for(int i=0;i<5;i++)
+    baseptr[i]->draw();
+}
