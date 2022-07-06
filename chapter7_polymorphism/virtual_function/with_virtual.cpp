e2e49186a6415a799cd9f13949026d13a2371b1e
chandansah0 <chandanprasadsah9@gmail.com>
2022-07-06 22:18:49 +0545

    virtual base

---
+++ chapter7_polymorphism/virtual_function/with_virtual.cpp
@@ -0,0 +1,56 @@
+// when a function is made virtual, it allows a program to decide at run time which function to call based on the TYPE OF THE OBJECT POINTED BY THE POINTER rather than TYPE OF THE POINTER ITSELF
+
+#include<iostream> 
+using namespace std;
+
+class shape{
+    protected:
+        int width,height;
+    public:
+        shape(int a=0, int b=0){
+            width = a;
+            height = b;
+        }
+
+        void virtual area(){
+            cout<<"Area of the shape: "<<endl;
+        }
+};
+
+class rectangle: public shape{
+    public:
+        rectangle(int a, int b){
+            width = a;
+            height =b;
+        }
+
+        void area(){
+            cout<<"The area of rectangle is: "<<(width*height)<<endl;
+        }
+};
+
+class triangle: public shape{
+    public:
+        triangle(int a, int b){
+            width = a;
+            height =b;
+        }
+
+        void area(){
+            cout<<"The area of triangle is: "<<(width*height/2)<<endl;
+        }
+};
+
+int main(){
+    shape *shap;
+    shape sh(1,2);
+    shap = &sh;
+    shap -> area();
+    rectangle rec(10,20);
+    shap = &rec;
+    shap ->area(); 
+    triangle tri(10,20);
+    shap = &tri;
+    shap ->area(); 
+    return 0;
+}
